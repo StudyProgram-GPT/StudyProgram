f5cd40fcb0fadfd38013a90ca849a7bc6e02e938
Macha_Nai <95839522+Macha-Nai@users.noreply.github.com>
2023-07-18 17:01:23 +0900

    app.pyにファイル保存機能を追加

--- app.py
+++ app.py
@@ -2,15 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/post', methods=['POST'])
 def post():
+    def save_as_c_file(text, filename):
+        with open(filename, 'w') as file:
+            file.write(text)
     code = request.form['code']
+
+    # Cファイルとして保存する
+    save_as_c_file(code, 'data.c')
     print(code)
     return 'Code received.'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
